b80da705df1bb3d130d982004e8ab434b4773f40
Edinson Rodriguez <edinson.rodriguez@utp.edu.co>
2024-10-08 19:40:35 -0500

    feat:Implementación completada y proyecto listo para su lanzamiento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { ProductListComponent } from './features/auth/Components/product-list/product-list.component';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'products', component: ProductListComponent, canActivate: [AuthGuard] },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+];
